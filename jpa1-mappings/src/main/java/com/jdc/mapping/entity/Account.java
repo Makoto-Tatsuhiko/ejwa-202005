e989aedd8d52b9c63a65956e067d10a3bb5c6443
Makoto-Tatsuhiko <htetlinoo.27@gmail.com>
2020-05-17 23:37:16 +0630

    Share Project

--- jpa1-mappings/src/main/java/com/jdc/mapping/entity/Account.java
+++ jpa1-mappings/src/main/java/com/jdc/mapping/entity/Account.java
@@ -2,16 +2,18 @@ package com.jdc.mapping.entity;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Date;
-
+import javax.persistence.Basic;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import lombok.Data;
 
@@ -23,18 +25,25 @@ public class Account implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	@Basic(fetch = FetchType.LAZY)
 	private String password;
 	private Gender gender;
 	@Enumerated(EnumType.STRING)
 	private Role role;
-	@Temporal(TemporalType.DATE)
 	private LocalDate dob;
 	private String address;
 	private String phone;
 	@Column(name = "login_email",unique = true,nullable = false)
 	private String email;
+	
+	@Transient
+	private boolean checked;
+	
+	@Embedded
+	private SecurityInfo security;
 	
 	public enum Gender {
 		Male, Female
